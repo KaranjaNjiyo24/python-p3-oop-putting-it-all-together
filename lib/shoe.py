e8b53a343f6787b550477ed9571dfdea2ff04efc
Andrew Karanja Njiyo <karanjaandrew2000@outlook.com>
2024-12-05 09:24:32 +0300

    initializes class with brand and size attributes

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+        self.brand = brand
+        self._size = size
+        self.condition = None
+
+    @property
+    def size(self):
+        return self._size
